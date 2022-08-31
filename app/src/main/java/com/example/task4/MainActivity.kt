f1b66b2d564a6ef40e1f17ff56ec71445f5aeca1
nadinnah <nadinahmedmohamed@gmail.com>
2022-08-31 20:53:21 +0300

    version 3

--- app/src/main/java/com/example/task4/MainActivity.kt
+++ app/src/main/java/com/example/task4/MainActivity.kt
@@ -3,51 +3,70 @@ package com.example.task4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var button:Button
-    private lateinit var button1:Button
-    private lateinit var button2:Button
-    private lateinit var button3:Button
-    private lateinit var button4:Button
-    private lateinit var button5:Button
-    private lateinit var button6:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //initializeView()
-    }
 
-//    private fun initializeView() {
-//        button = findViewById<Button>(R.id.button)
-//        button.setOnClickListener {
-//
-//        }
-//        button = findViewById<Button>(R.id.button1)
-//        button.setOnClickListener {
-//
-//        }
-//        button = findViewById<Button>(R.id.button2)
-//        button.setOnClickListener {
-//
-//        }
-//        button = findViewById<Button>(R.id.button3)
-//        button.setOnClickListener {
-//
-//        }
-//        button = findViewById<Button>(R.id.button4)
-//        button.setOnClickListener {
-//
-//        }
-//        button = findViewById<Button>(R.id.button5)
-//        button.setOnClickListener {
-//
-//        }
-//        button = findViewById<Button>(R.id.button6)
-//        button.setOnClickListener {
-//
-//        }
-//
+        val zero:TextView= findViewById(R.id.zero)
+
+        val zero2:TextView= findViewById(R.id.zero2)
+
+
+        val plus2:Button= findViewById(R.id.plus2)
+        plus2.setOnClickListener {
+            val currentScore= zero.text.toString().toInt()
+            val newScore= currentScore + 2
+            zero.text= "$newScore"
+        }
+        val plus2a:Button= findViewById(R.id.plus2a)
+        plus2a.setOnClickListener {
+            val currentScore= zero2.text.toString().toInt()
+            val newScore= currentScore + 2
+            zero2.text= "$newScore"
+        }
+
+        val plus3:Button= findViewById(R.id.plus3)
+        plus3.setOnClickListener {
+            val currentScore= zero.text.toString().toInt()
+            val newScore= currentScore + 3
+            zero.text= "$newScore"
+        }
+        val plus3a:Button= findViewById(R.id.plus3a)
+        plus3a.setOnClickListener {
+            val currentScore= zero2.text.toString().toInt()
+            val newScore= currentScore + 3
+            zero2.text= "$newScore"
+        }
+
+        val freethrow1:Button= findViewById(R.id.freethrow1)
+        freethrow1.setOnClickListener {
+            val currentScore= zero.text.toString().toInt()
+            val newScore= currentScore + 1
+            zero.text= "$newScore"
+        }
+
+        val freethrow2:Button= findViewById(R.id.freethrow2)
+        freethrow2.setOnClickListener {
+            val currentScore= zero2.text.toString().toInt()
+            val newScore= currentScore + 1
+            zero2.text= "$newScore"
+        }
+
+        val reset:Button= findViewById(R.id.reset)
+        reset.setOnClickListener {
+
+            zero.text= "0"
+            zero2.text= "0"
+        }
+
+    }
+//    private fun addPoints(){
+//        val currentScore= plus2.text.toString().toInt()
+//        val newScore= currentScore + 2
+//        zero.text= "$newScore"
 //    }
 }

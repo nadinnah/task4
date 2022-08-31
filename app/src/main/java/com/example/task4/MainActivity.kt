713b3ef43b68cc3991218cbfe230b4dea7c57a41
nadinnah <nadinahmedmohamed@gmail.com>
2022-08-31 21:34:45 +0300

    version 4

--- app/src/main/java/com/example/task4/MainActivity.kt
+++ app/src/main/java/com/example/task4/MainActivity.kt
@@ -12,61 +12,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val zero:TextView= findViewById(R.id.zero)
-
         val zero2:TextView= findViewById(R.id.zero2)
 
-
         val plus2:Button= findViewById(R.id.plus2)
-        plus2.setOnClickListener {
-            val currentScore= zero.text.toString().toInt()
-            val newScore= currentScore + 2
-            zero.text= "$newScore"
-        }
+        plus2.setOnClickListener {addPoints(2,zero)}
         val plus2a:Button= findViewById(R.id.plus2a)
-        plus2a.setOnClickListener {
-            val currentScore= zero2.text.toString().toInt()
-            val newScore= currentScore + 2
-            zero2.text= "$newScore"
-        }
+        plus2a.setOnClickListener {addPoints(2,zero2)}
 
         val plus3:Button= findViewById(R.id.plus3)
-        plus3.setOnClickListener {
-            val currentScore= zero.text.toString().toInt()
-            val newScore= currentScore + 3
-            zero.text= "$newScore"
-        }
+        plus3.setOnClickListener {addPoints(3,zero)}
         val plus3a:Button= findViewById(R.id.plus3a)
-        plus3a.setOnClickListener {
-            val currentScore= zero2.text.toString().toInt()
-            val newScore= currentScore + 3
-            zero2.text= "$newScore"
-        }
+        plus3a.setOnClickListener {addPoints(3,zero2)}
 
         val freethrow1:Button= findViewById(R.id.freethrow1)
-        freethrow1.setOnClickListener {
-            val currentScore= zero.text.toString().toInt()
-            val newScore= currentScore + 1
-            zero.text= "$newScore"
-        }
+        freethrow1.setOnClickListener {addPoints(1,zero)}
 
         val freethrow2:Button= findViewById(R.id.freethrow2)
-        freethrow2.setOnClickListener {
-            val currentScore= zero2.text.toString().toInt()
-            val newScore= currentScore + 1
-            zero2.text= "$newScore"
-        }
+        freethrow2.setOnClickListener { addPoints(1,zero2) }
 
         val reset:Button= findViewById(R.id.reset)
         reset.setOnClickListener {
-
             zero.text= "0"
             zero2.text= "0"
         }
 
     }
-//    private fun addPoints(){
-//        val currentScore= plus2.text.toString().toInt()
-//        val newScore= currentScore + 2
-//        zero.text= "$newScore"
-//    }
+    private fun addPoints(points: Int, scoreLabel: TextView){
+        val currentScore= scoreLabel.text.toString().toInt()
+        val newScore= currentScore + points
+        scoreLabel.text= "$newScore"
+    }
 }
